4abfcc5df6946a0bccfcf1d51d747b74c1b23b49
Stefan Bossbaly <Sbossb@gmail.com>
2012-04-19 11:52:35 -0400

    Only update when the number has changed

--- project/DisplayForm.cs
+++ project/DisplayForm.cs
@@ -14,6 +14,7 @@ namespace project
 		private Thread readThread;
 		private SerialPort port;
 		private bool running;
+		private int lastImage = -126;
 		
 		public DisplayForm(String portName)
 		{
@@ -46,28 +47,39 @@ namespace project
 				try
 				{
 					int number = port.ReadByte();
-					switch (number)
+					if (number == lastImage)
 					{
-						case 48:
-							pictureBox.Image = Image.FromFile("C:\\Images\\evil_lair.png");
-							break;
-						case 49:
-							pictureBox.Image = Image.FromFile("C:\\Images\\floor1.jpg");
-							break;
-						case 50:
-							pictureBox.Image = Image.FromFile("C:\\Images\\floor2.jpg");
-							break;
-						case 51:
-							pictureBox.Image = Image.FromFile("C:\\Images\\floor1.jpg");
-							break;
-						case 52:
-							pictureBox.Image = Image.FromFile("C:\\Images\\depth_charge.jpg");
-							break;
-						case 53:
-							pictureBox.Image = Image.FromFile("C:\\Images\\floor2.jpg");
-							break;
-						default:
-							break;
+						//Do nothing
+					}
+					else if (number == 48)
+					{
+						pictureBox.Image = Image.FromFile("C:\\Images\\evil_lair.png");
+						lastImage = 48;
+					}
+					else if (number == 49)
+					{
+						pictureBox.Image = Image.FromFile("C:\\Images\\floor1.jpg");
+						lastImage = 49;
+					}
+					else if (number == 50)
+					{
+						pictureBox.Image = Image.FromFile("C:\\Images\\floor2.jpg");
+						lastImage = 50;
+					}
+					else if (number == 51)
+					{
+						pictureBox.Image = Image.FromFile("C:\\Images\\floor3.jpg");
+						lastImage = 51;
+					}
+					else if (number == 52)
+					{
+						pictureBox.Image = Image.FromFile("C:\\Images\\depth_charge.jpg");
+						lastImage = 52;
+					}
+					else if (number == 53)
+					{
+						pictureBox.Image = Image.FromFile("C:\\Images\\floor4.jpg");
+						lastImage = 53;
 					}
 				}
 				catch (TimeoutException)
